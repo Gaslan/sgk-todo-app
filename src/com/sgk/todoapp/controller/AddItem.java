10a003ffb726067cb7b13a4a5fcbfd3df5e6d184
ibrahim <bilalcanakar@gmail.com>
2020-08-14 09:19:20 +0300

    Güncelleme fonksiyonu eklendi.

--- src/com/sgk/todoapp/controller/AddItem.java
+++ src/com/sgk/todoapp/controller/AddItem.java
@@ -1,27 +1,36 @@
 package com.sgk.todoapp.controller;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sgk.todoapp.model.TodoDvo;
 import com.sgk.todoapp.repository.TodoMongoDbRepository;
 
-
-@WebServlet("/delete")
-public class DeleteItem extends HttpServlet {
+@WebServlet("/add")
+public class AddItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public DeleteItem() {
+    public AddItem() {
         super();
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("_id");
+		String newItem = request.getParameter("addedItem");
+		
+		TodoDvo todo = new TodoDvo();
+		todo.setTitle("deneme");
+		todo.setDetail(newItem);
+		todo.setStatus(1);
+		todo.setCreationDate(LocalDateTime.now());
+		
 		TodoMongoDbRepository dbRepo = new TodoMongoDbRepository();
-		dbRepo.delete(id);
+		dbRepo.create(todo);
 	}
 
 }
--- src/com/sgk/todoapp/controller/UpdateItem.java
+++ src/com/sgk/todoapp/controller/UpdateItem.java
@@ -1,27 +1,39 @@
 package com.sgk.todoapp.controller;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sgk.todoapp.model.TodoDvo;
 import com.sgk.todoapp.repository.TodoMongoDbRepository;
 
 
-@WebServlet("/delete")
-public class DeleteItem extends HttpServlet {
+@WebServlet("/update")
+public class UpdateItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public DeleteItem() {
+    public UpdateItem() {
         super();
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("_id");
+		String updatedJob = request.getParameter("updatedJob");
+		String _id = request.getParameter("_id");
+		
+		TodoDvo todo = new TodoDvo();
+		todo.setId(_id);
+		todo.setTitle("deneme");
+		todo.setDetail(updatedJob);
+		todo.setStatus(1);
+		todo.setCreationDate(LocalDateTime.now());
+		
 		TodoMongoDbRepository dbRepo = new TodoMongoDbRepository();
-		dbRepo.delete(id);
+		dbRepo.update(todo);
 	}
 
 }
